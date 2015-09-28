db73dd83b5331ea230866813039dc7f722c28eaf
Zheng Li <loylizh@gmail.com>
2015-09-28 12:08:07 +0800

    add animation

--- app/components/Todos/index.js
+++ app/components/Todos/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Todo from '../Todo';
 
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import './styles.css';
 
 export default class Todos extends React.Component {
 	render() {
 		return (
 			<ul className="todos">
-				{ this.props.todos.map( todo => <Todo key={todo.id} todo={todo} /> ) }
+				<ReactCSSTransitionGroup transitionName="todo" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
+					{ this.props.todos.map( todo => <Todo key={todo.id} todo={todo} /> ) }
+				</ReactCSSTransitionGroup>
 			</ul>
 		);
 	}
